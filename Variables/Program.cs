2ea5edeae3be6e4c74b0b99e2fed6519b0a5dee1
codywashbrook <codywashbrook@hotmail.com>
2022-09-20 09:42:29 -0300

    Updated

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,6 +10,44 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Variables");
+            Console.WriteLine("");
+
+            Console.WriteLine("Integers");
+            int score; //declaration that score is intiger// int = whole number
+            score = 0; //initilization == init, "score" to be 0
+            int enemyPoints;
+            enemyPoints = 100;
+            Console.WriteLine("Enemy Points: " + enemyPoints);
+            score = score + enemyPoints; // when enemy killed score is +100
+            Console.WriteLine("Score: " + score); //display score
+
+            Console.WriteLine();
+
+            Console.WriteLine("Floats");
+            // floating points == floats are numbers w/ decimals
+            float lapTime; //declaration
+            lapTime = 0.0f; // f = floating point
+            Console.WriteLine(lapTime);
+            lapTime = 34.67f;
+            Console.WriteLine(lapTime);
+
+            Console.WriteLine();
+
+            //string = text
+            Console.WriteLine("Strings");
+
+            string title; // declaration
+            title = "Cody Washbrook";
+            Console.WriteLine(title);
+            title = "Wody Cashbrook";
+            Console.WriteLine(title);
+
+            Console.ReadKey(true);
+
+
+
         }
     }
 }
