04894e40539f3c5b57044e26ae3c44c5b61cbf30
codywashbrook <codywashbrook@hotmail.com>
2022-09-20 10:07:50 -0300

    Updated
    including colour!

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,11 +10,16 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Variables");
+            Console.ResetColor();
             Console.WriteLine("");
 
+            Console.BackgroundColor = ConsoleColor.Blue; 
+            Console.ForegroundColor = ConsoleColor.White; //after this line every text will be white on blue background
             Console.WriteLine("Integers");
+            Console.ResetColor(); //reset to the default colour
             int score; //declaration that score is intiger// int = whole number
             score = 0; //initilization == init, "score" to be 0
             int enemyPoints;
@@ -25,7 +30,10 @@ namespace Variables
 
             Console.WriteLine();
 
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Floats");
+            Console.ResetColor();
             // floating points == floats are numbers w/ decimals
             float lapTime; //declaration
             lapTime = 0.0f; // f = floating point
@@ -36,7 +44,10 @@ namespace Variables
             Console.WriteLine();
 
             //string = text
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Strings");
+            Console.ResetColor();
 
             string title; // declaration
             title = "Cody Washbrook";
